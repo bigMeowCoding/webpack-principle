bce51451dd8a3315bc87081e53694e0d0b7177ef
zhouyijun <zhou495248579@hotmail.com>
2021-02-20 14:36:44 +0800

    make dir

--- bundle_2.ts
+++ bundle_2.ts
@@ -1,12 +1,9 @@
-// 请确保你的 Node 版本大于等于 14
-// 请先运行 yarn 或 npm i 来安装依赖
-// 然后使用 node -r ts-node/register 文件路径 来运行，
-// 如果需要调试，可以加一个选项 --inspect-brk，再打开 Chrome 开发者工具，点击 Node 图标即可调试
 import * as babel from "@babel/core";
 import { parse } from "@babel/parser";
 import traverse from "@babel/traverse";
-import { readFileSync, writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFile } from "fs";
 import { resolve, relative, dirname } from "path";
+import * as fs from "fs";
 
 // 设置根目录
 const projectRoot = resolve(__dirname, "project_2");
@@ -64,9 +61,18 @@ function generateCode() {
   `;
   return code;
 }
-writeFileSync("dist_2.js", generateCode());
-console.log(depRelation);
-console.log("done");
+const existDist = existsSync("./dist");
+if (!existDist) {
+  fs.mkdirSync("./dist");
+}
+
+writeFile("./dist/dist_2.js", generateCode(), (err) => {
+  if (err) {
+    return;
+  }
+  console.log("done");
+});
+// console.log(depRelation);
 
 /**
  *
